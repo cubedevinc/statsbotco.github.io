6fcfd14adab778c02d41c15c821cfdc50f200d4e
Artyom Keydunov <artyom.keydunov@gmail.com>
2019-03-09 18:32:28 +0300

    Update event analytics examplee

---
+++ cubejs/event-analytics/precache-manifest.0805cc346d2f0035e07eebcac9b2230f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d089d084de6536dfcf992e782c2cede",
+    "url": "/cubejs/event-analytics/static/media/schema.9d089d08.png"
+  },
+  {
+    "revision": "93ee5abe5191531985af",
+    "url": "/cubejs/event-analytics/static/js/runtime~main.93ee5abe.js"
+  },
+  {
+    "revision": "7bdd3e6f6a7923680437",
+    "url": "/cubejs/event-analytics/static/js/main.7bdd3e6f.chunk.js"
+  },
+  {
+    "revision": "a835a9621b49375baaab",
+    "url": "/cubejs/event-analytics/static/js/1.a835a962.chunk.js"
+  },
+  {
+    "revision": "7bdd3e6f6a7923680437",
+    "url": "/cubejs/event-analytics/static/css/main.c2817a9b.chunk.css"
+  },
+  {
+    "revision": "668184c99685115b0accdbdb221aa6de",
+    "url": "/cubejs/event-analytics/index.html"
+  }
+];
--- cubejs/event-analytics/service-worker.js
+++ cubejs/event-analytics/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cubejs/event-analytics/precache-manifest.67bdec2c46f1072fbc1ee391a654c5b7.js"
+  "/cubejs/event-analytics/precache-manifest.0805cc346d2f0035e07eebcac9b2230f.js"
 );
 
 workbox.clientsClaim();
