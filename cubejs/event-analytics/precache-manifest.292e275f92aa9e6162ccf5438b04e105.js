30bb893ba81995d526b868d9313733ab6f2eaf95
Pavel Tiunov <pavel.tiunov@gmail.com>
2019-03-09 14:06:08 +0300

    Event analytics

---
+++ cubejs/event-analytics/precache-manifest.292e275f92aa9e6162ccf5438b04e105.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "feac3274961a1517cc7c",
+    "url": "/static/js/main.feac3274.chunk.js"
+  },
+  {
+    "revision": "981c69a36232a6d5a7d9",
+    "url": "/static/js/1.981c69a3.chunk.js"
+  },
+  {
+    "revision": "2e4329640bb260dc4827648d6d1564de",
+    "url": "/index.html"
+  }
+];
