deef208170b6d7c1c07056b202fe24252ff31653
Artyom Keydunov <artyom.keydunov@gmail.com>
2019-03-28 11:52:33 +0300

    Add React Dashboard example

---
+++ cubejs/react-dashboard/precache-manifest.0a1796f45000b8159c894e1fe3bbcd00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "263f71b5d90835619624",
+    "url": "/static/js/main.dc7cb144.chunk.js"
+  },
+  {
+    "revision": "77e0d8f0f83c26eb6f0a",
+    "url": "/static/js/2.1c43712a.chunk.js"
+  },
+  {
+    "revision": "263f71b5d90835619624",
+    "url": "/static/css/main.79e12ce7.chunk.css"
+  },
+  {
+    "revision": "77e0d8f0f83c26eb6f0a",
+    "url": "/static/css/2.a6340104.chunk.css"
+  },
+  {
+    "revision": "5e46c5d1c3bec5b2e0b0e07fb5498483",
+    "url": "/index.html"
+  }
+];
