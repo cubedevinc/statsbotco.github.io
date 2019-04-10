0d1e36e9d2e7bf67118404fc3c25b8002437ef4d
Timur Minulin <timur@statsbot.co>
2019-04-10 18:12:30 +0300

    add nginx analytics dashboard page

---
+++ cubejs/nginx-analytics-dashboard/precache-manifest.61cdf2ba8978bbd3fd160ef723c38626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fdb1b8e70a8d4ca6bea",
+    "url": "/cubejs/nginx-analytics-dashboard/static/js/runtime~main.fbc69acc.js"
+  },
+  {
+    "revision": "91b171dd0d1063bdaf28",
+    "url": "/cubejs/nginx-analytics-dashboard/static/js/main.4c728686.chunk.js"
+  },
+  {
+    "revision": "eecc4b0a24a7e480226d",
+    "url": "/cubejs/nginx-analytics-dashboard/static/js/2.ea880417.chunk.js"
+  },
+  {
+    "revision": "91b171dd0d1063bdaf28",
+    "url": "/cubejs/nginx-analytics-dashboard/static/css/main.bbc8dbdd.chunk.css"
+  },
+  {
+    "revision": "eecc4b0a24a7e480226d",
+    "url": "/cubejs/nginx-analytics-dashboard/static/css/2.a47ebc4d.chunk.css"
+  },
+  {
+    "revision": "01ac46a984ba1f623570273e9b4182a1",
+    "url": "/cubejs/nginx-analytics-dashboard/index.html"
+  }
+];
